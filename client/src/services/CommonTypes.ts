a933e2227175f7b68754dfe4b7f37ae4df57b37e
ling-zer <1113231244@qq.com>
2022-06-22 10:25:45 +0800

    客户端api请求

---
+++ client/src/services/CommonTypes.ts
@@ -0,0 +1,21 @@
+export interface IResponseError {
+    err: string,
+    data: null
+}
+
+export interface IResponseData<T> {
+    err: null,
+    data: T
+}
+
+export interface IResponsePageData<T> {
+    err: null,
+    total: number,
+    data: T[]
+}
+
+export interface ISearchCondtion {
+    page?: number,
+    limit?: number,
+    key?: string
+}
