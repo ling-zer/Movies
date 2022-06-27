e8ecc345843da6614f911e70d03da5c2c2a4db0f
ling-zer <1113231244@qq.com>
2022-06-27 17:52:14 +0800

    添加电影

--- client/src/pages/movie/AddMovie.tsx
+++ client/src/pages/movie/AddMovie.tsx
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import MovieForm from '../../components/MovieForm'
+import { MovieService } from '../../services/MovieService'
 
 export default class AddMovie extends Component {
+
+
   render() {
     return (
-      <div>AddMovie</div>
+      <MovieForm onSubmit={async(movie) => {
+        const resp = await MovieService.add(movie)
+        if(resp.data) {
+          return ""
+        } else {
+          return resp.err
+        }
+      }}/>
     )
   }
 }
