0eb63ba5ba7b1490e725e385e43910c3918f5913
ling-zer <1113231244@qq.com>
2022-10-17 11:20:15 +0800

    路由懒加载

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import Layout from './pages/Layout';
-import MovieList from './pages/movie/MovieList';
-import AddMovie from './pages/movie/AddMovie';
-import EditMovie from './pages/movie/EditMovie';
+import React, { Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
+import Layout from './pages/Layout';
+
+const Home = React.lazy(() => import('./pages/Home'))
+// const Layout = React.lazy(() => import('./pages/Layout'))
+const MovieList = React.lazy(() => import('./pages/movie/MovieList'))
+const AddMovie = React.lazy(() => import('./pages/movie/AddMovie'))
+const EditMovie = React.lazy(() => import('./pages/movie/EditMovie'))
 
 const App: React.FC = () => {
-  
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={ <Layout/> }>
-          <Route index element={<Home/>}></Route>
-          <Route path='/movie' element={<MovieList/>}></Route>
-          <Route path='/movie/add' element={<AddMovie/>}></Route>
-          <Route path='/movie/edit/:id' element={<EditMovie/>}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Suspense fallback={<div>Loading...</div>}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route path='/movie' element={<MovieList />}></Route>
+            <Route path='/movie/add' element={<AddMovie />}></Route>
+            <Route path='/movie/edit/:id' element={<EditMovie />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Suspense>
   );
 }
 
